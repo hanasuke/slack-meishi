17fb57599b233b7ecadc8fce4ad3262ee4c9279d
naosuke <info@naosuke.me>
2023-11-03 21:14:20 +0900

    impl getting user info from slack

---
+++ libs/person.py
@@ -0,0 +1,17 @@
+import io
+import requests
+
+from PIL import Image
+from dataclasses import dataclass
+
+@dataclass
+class Person:
+  icon: Image
+  username: str
+  realname: str
+
+  def __init__(self, icon_url: str, username: str, realname:str):
+    self.username = username
+    self.realname = realname
+
+    self.icon = Image.open(io.BytesIO(requests.get(icon_url).content))
